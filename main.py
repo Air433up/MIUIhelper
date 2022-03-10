db60bb7a6ba4caea71f2209457077c7a16475fc6
AzurStar <tuan_kay@outlook.com>
2022-03-10 11:12:04 +0800

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ with open("data/accounts.json", "r", encoding="utf-8") as r:
 for Account in accounts:
     correct = {}
     Answer(account=Account["account"], password=Account["password"])
+    print("\n正在收集题目数据...")
     lst = GetList(account=Account["account"], password=Account["password"])
     for qu in lst:
         correctSelect = qu["correctSelect"]
@@ -16,8 +17,10 @@ for Account in accounts:
             if option["optionId"] == correctSelect:
                 Roption = option["text"]
         correct[question] = Roption
-        print("正在收集题目数据...")
     with open("data/correct.json", "r", encoding="utf-8") as r:data = json.load(r)
     data = dict(correct, **data)
     with open("data/correct.json", "w", encoding="utf-8") as w:
         json.dump(data, fp=w, indent=2)
+        print("题目收集完成！")
+    print("开始尝试寻找其他账号...")
+print("\n结束！")
--- web.py
+++ web.py
@@ -1,16 +1,17 @@
 import json
-from multiprocessing.connection import wait
 import random,requests
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+prefs = {'profile.managed_default_content_settings.images': 2}
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_experimental_option('prefs',prefs)
 
 wait_time = 10 
 answer_time = 5
@@ -22,6 +23,7 @@ def Answer(account, password):
 
     with open("data/correct.json", "r", encoding="utf-8") as r:
         crrect = json.load(r)
+        
     crrect = dict(crrect, **correctGitee)
 
     with open("data/keywords.json", "r", encoding="utf-8") as r:
@@ -38,19 +40,18 @@ def Answer(account, password):
 
     WebDriverWait(MIUI, wait_time).until(EC.visibility_of_element_located((By.TAG_NAME, "section")))  # 等待进入界面
     print("已进入答题页面！")
-    try:
-        WebDriverWait(MIUI, wait_time).until(EC.visibility_of_element_located((By.CLASS_NAME, "DailyQuestions_start__2h7_C"))).click()
-    except:
-        pass
+    try:WebDriverWait(MIUI, wait_time).until(EC.visibility_of_element_located((By.CLASS_NAME, "DailyQuestions_start__2h7_C"))).click()
+    except:pass
     print("准备开始答题...")
-    for ty in range(10):
+    for ty in range(5):
         print("\r尝试次数:",ty+1,end="",flush=True)
         try:
-            MIUI.refresh()
+            try:WebDriverWait(MIUI, answer_time).until(EC.visibility_of_element_located((By.CLASS_NAME, "DailyQuestions_start__2h7_C"))).click()
+            except:pass
             question = WebDriverWait(MIUI, answer_time).until(EC.visibility_of_element_located((By.CLASS_NAME, "DailyQuestions_title__3WufQ"))).text
             options = WebDriverWait(MIUI, answer_time).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "DailyQuestions_option__WTHY5")))
 
-            print(question)
+            print("\n",question)
             t = 0
             for option in options:
                 t+=1
@@ -76,12 +77,12 @@ def GetList(account, password):
 
     def login(Lst: webdriver.Chrome, account, password):
         Lst.get(url=lst["loginUrl"])
-        WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, "密码登录"))).click()
-        WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.NAME, "account"))).send_keys(account)
-        WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.NAME, "password"))).send_keys(password)
-        WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-checkbox"))).click()
-        WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "button"))).click()
-        r = WebDriverWait(Lst, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "pre"))).text
+        WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.LINK_TEXT, "密码登录"))).click()
+        WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.NAME, "account"))).send_keys(account)
+        WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.NAME, "password"))).send_keys(password)
+        WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-checkbox"))).click()
+        WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.TAG_NAME, "button"))).click()
+        r = WebDriverWait(Lst, wait_time).until(EC.visibility_of_element_located((By.TAG_NAME, "pre"))).text
         r = json.loads(r)
         return r
 
