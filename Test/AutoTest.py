1e93c87a138a7947fc60158b4a4ce5840ef9ba6c
AzurStar <tuan_kay@outlook.com>
2022-03-17 15:07:24 +0800

    Update Data

--- Test/AutoTest.py
+++ Test/AutoTest.py
@@ -1,7 +1,7 @@
 import json
 from AutoWeb import internalTest
 
-with open("Test/data/account.json", "r", encoding="utf-8") as r:accounts = json.load(r)
+with open("Test/data/accounts.json", "r", encoding="utf-8") as r:accounts = json.load(r)
 
 for Account in accounts:
     internalTest(account=Account["account"], password=Account["password"],tasks=Account["tasks"])
--- Test/check.py
+++ Test/check.py
@@ -4,12 +4,12 @@ def check(task):
     filenames = {"开发版公测":"DevelopmentPublicTest","开发版内测":"DevelopmentInternalTest","稳定版内测":"StableInternalTest"}
     with open(f"Test/data/Correct{filenames[task]}.json", "r", encoding="utf-8") as r:Correct = set(json.load(r))
     with open(f"Test/data/{filenames[task]}.json", "r", encoding="utf-8") as r:data:dict = json.load(r)
-    print(f"Data:{len(data)}")
-    print(f"Correct:{len(Correct)}")
     for question in data:
         if question not in Correct:
             print(question)
             for opt in data[question]:print(opt)
             print()
+    print(f"Data:{len(data)}")
+    print(f"Correct:{len(Correct)}")
 
-check("稳定版内测")
+check("开发版公测")
